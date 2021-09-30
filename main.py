8f5a1b7bf2943439cf4efe356e98d5784f0a7f4b
Bordianu Raul <raul.bordianu@stud.ubbcluj.ro>
2021-09-30 19:40:38 +0300

    is_prime rezolvat

--- main.py
+++ main.py
@@ -4,16 +4,14 @@ Returneaza true daca n este prim si false daca nu.
 def is_prime(n):
   # codul vostru aici
 
-  n=int(input("Introdu numarul: "))
+  if n<2:
+    return False
+  for i in range(2, n/2+1):
 
-  prim= true
-  for i in range(2, n/2):
+      if n % i == 0:
+        return False
 
-      if (n % i) == 0:
-        prim= false
-        break;
-
-  return prim
+  return True
 
 
 
@@ -22,8 +20,9 @@ Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
   # codul vostru aici
-  
-  
+
+
+
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
@@ -32,16 +31,32 @@ def get_cmmdc_v1(x, y):
 
 
   # comentariu
-  
+
 '''
 Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
 def get_cmmdc_v2(x, y):
   # codul vostru aici
-  
-  
+
+
 def main():
   # interfata de tip consola aici
+
+  while True:
+    print("1. Primalitatea lui n")
+    print("x. Iesire din program")
+
+    optiune=input("Alege optiune:")
+
+    if optiune == '1':
+      numar=int(input("Introdu numarul: "))
+      if is_prime(numar):
+        print(f'{numar} este numar prim')
+      else: print(f'{numar} nu este numar prim')
+
+    elif optiune == 'x':
+        break
+    else: print("Optiune invalida")
 
 if __name__ == '__main__':
   main()
